1716c057229f6b068abba1c97e060a0792d6cae4
Ian <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2022-01-06 14:32:04 -0500

    upd

--- degrunk-data.py
+++ degrunk-data.py
@@ -10,6 +10,9 @@ import json
 from binance.client import Client
 import binance.enums
 from datetime import date
+import dateparser
+import pytz
+from datetime import datetime
 
 def date_to_milliseconds(date_str):
     """Convert UTC date to milliseconds
@@ -82,87 +85,268 @@ def getstate(outlist, exchangeinfo, tokenname = 'USDT'):
                 currlist.append(ab['symbol'])
     return currlist
 
-def download(start, end, interval, currlist, pth, type):    
+def download(start, end, interval, currlist, pth, type, withcgdata=True, withaggtrades=False, aggtradelimit=0):    
     from binance.enums import HistoricalKlinesType
-    cg = CoinGeckoAPI()
-    aa = cg.get_coins_list(include_platform='false')
+    if withcgdata == True:
+        cg = CoinGeckoAPI()
+        aa = cg.get_coins_list()
     for token in currlist:
-        ccdcdc = date_to_milliseconds(start)//1000 + 86400
-        ccdcdd = date_to_milliseconds(end)//1000
-        tokid = {}
-        b = token.split('.')[0].split('USDT')[0].lower()
-        for tokn in aa:
-            if tokn['symbol'] == b:
-                tokid[token] = tokn['id']
-        #for a in range(delt.days):
-        try:
-            cval = cg.get_coin_market_chart_range_by_id(id=tokid[token], vs_currency="usd", from_timestamp=ccdcdc, to_timestamp=ccdcdd)
-        except:
-            continue  
-        d1 = [item[1] for item in cval['market_caps']]
-        d2 = [item[1] for item in cval['total_volumes']]
-        d3 = [item[1] for item in cval['prices']]
-        klines = client.get_historical_klines_generator(token, interval, start, end,  klines_type= HistoricalKlinesType.SPOT)
-        with open(os.path.join(pth,'{}.csv'.format(token)), 'w', newline='') as f:
-            writerc = csv.writer(f)
-            dic = ['date', 'open', 'high','low', 'close','volume','symbol','QAV','numberoftrades','takerbuyBAV','takerbuyQAV','market_cap', 'total_volume','price_from_coingecko', 'factor']
-            for enumr, qqq in enumerate(range(1000)):
-                dic.append('p_{}'.format(enumr))
-                dic.append('q_{}'.format(enumr))
-                dic.append('m_{}'.format(enumr))
-                dic.append('M_{}'.format(enumr))
-            writerc.writerow(dic)
-            gct = 0
-            it = 0
-            prevdate = 0
-            cnct = 0
-            
-            for a in klines:
-                olis = []
-                agg_trades = client.aggregate_trade_iter(symbol=token, start_str=a[0], endingm=a[6], day=True)
-                cvl = 0
-                if datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%d") != prevdate:
-                    cnct = cnct + 1
-                    prevdate = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%d")
-                for bb in agg_trades:
-                    for bbb in bb:
-                        if bbb == []:
-                            continue
-                        for bbbbb in bbb:
-                            # tvl = float(bbbbb['p'])*float(bbbbb['q'])
-                            olis.append(bbbbb)
-                fnlis = sorted(olis, key = lambda i: i['q'], reverse=True)[0:1000]
-                
-                if type == 'day':
-                    dd = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%Y-%m-%d")
-                if type == '1min':
-                    dd = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%Y-%m-%d %H:%M:%S")
-                if d1 == []:
-                    b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],None,d2[cnct],d3[cnct],1]
-                if d2 == []:
-                    b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],d1[cnct],None,d3[cnct],1]
-                if d3 == []:
-                    b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],d1[cnct],d2[cnct],None,1]
-                else:
-                    b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],d1[cnct],d2[cnct],d3[cnct],1]
-                
-                for enumr, qqq in enumerate(range(1000)):
+        if withcgdata == True:
+            ccdcdc = date_to_milliseconds(start)//1000 #+ 86400
+            ccdcdd = date_to_milliseconds(end)//1000 + 3600
+            ccdccc = date_to_milliseconds(end)//1000
+            tokid = {}
+            b = token.split('.')[0].split('USDT')[0].lower()
+            for tokn in aa:
+                if tokn['symbol'] == b:
+                    tokid[token] = tokn['id']
+            try:
+                cval = cg.get_coin_market_chart_range_by_id(id=tokid[token], vs_currency="usd", from_timestamp=ccdcdc, to_timestamp=ccdcdd)
+            except:
+                try:
+                    cval = cg.get_coin_market_chart_range_by_id(id=token.split('USDT')[0].lower(), vs_currency="usd", from_timestamp=ccdcdc, to_timestamp=ccdcdd)
+                except Exception as e: 
+                    
                     try:
-                        b.append(fnlis[enumr]['p'])
-                        b.append(fnlis[enumr]['q'])
-                        b.append(fnlis[enumr]['m'])
-                        b.append(fnlis[enumr]['M'])
+                        if e.response.status_code == 429:
+                            time.sleep(60)
+                            cval = cg.get_coin_market_chart_range_by_id(id=token.split('USDT')[0].lower(), vs_currency="usd", from_timestamp=ccdcdc, to_timestamp=ccdcdd)
                     except:
-                        b.append(None)
-                        b.append(None)
-                        b.append(None)
-                        b.append(None)
-                writerc.writerow(b)
-                gct = gct + 1
-                if gct == 1000:
-                    it = it + 1
-                    gct= 0
-                    print('{} Iteration #: '.format(token), it)
+                        continue
+                    
+                    #print('skipping', token)
+                    
+             
+                
+            
+            # +
+            #     try:
+            #         print('request failed, waiting 60s')
+                    
+            #         cval = cg.get_coin_market_chart_range_by_id(id=tokid[token], vs_currency="usd", from_timestamp=ccdcdc, to_timestamp=ccdcdd)
+            #     except:
+            #         try:
+            #             cval = cg.get_coin_market_chart_range_by_id(id=b, vs_currency="usd", from_timestamp=ccdcdc, to_timestamp=ccdcdd)
+            #         except:
+        klines = client.get_historical_klines(token, interval, start, end,  klines_type= HistoricalKlinesType.SPOT)
+                    
+        # try:    
+        #     klines = client.get_historical_klines(token, interval, ccdcdc, ccdccc,  klines_type= HistoricalKlinesType.SPOT)
+        # except:
+        #     print('waiting on ', token)
+        #     time.sleep(60)
+        #     klines = client.get_historical_klines(token, interval, ccdcdc, ccdccc,  klines_type= HistoricalKlinesType.SPOT)
+
+        if not klines == []:
+            klines.pop(-1)
+        else:
+            continue
+        if len(cval['market_caps']) != 0:
+            d1 = [item[1] for item in cval['market_caps']]
+            # if type == 'day': 
+            #     clis = []
+            #     ct = 0
+            #     cnt = 0
+            #     for v in d1:
+            #         cnt = cnt + 1
+            #         ct = ct + v
+            #         if cnt == 24:
+            #             ct = ct/24
+            #             clis.append(ct)
+            #             ct = 0
+            #     d1 = clis
+        else:
+            d1 = []
+            d1.extend([0] * len(klines))
+        if len(cval['total_volumes']) != 0:
+            d2 = [item[1] for item in cval['total_volumes']]
+            # if type == 'day': 
+            #     clis = []
+            #     ct = 0
+            #     cnt = 0
+            #     for v in d2:
+            #         cnt = cnt + 1
+            #         ct = ct + v
+            #         if cnt == 24:
+            #             clis.append(ct)
+            #             ct = 0
+            #     d2 = clis
+        else: 
+            d2 = []
+            d2.extend([0] * len(klines))
+        if len(cval['prices']) != 0:
+            d3 = [item[1] for item in cval['prices']]
+            # if type == 'day':
+            #     clis = []
+            #     ct = 0
+            #     cnt = 0
+            #     for v in d3:
+            #         cnt = cnt + 1
+            #         ct = ct + v
+            #         if cnt == 24:
+            #             ct = ct/24
+            #             clis.append(ct)
+            #             ct = 0
+            #     d3 = clis
+        else:
+            d3 = []
+            d3.extend([0] * len(klines))
+        if withcgdata == True:
+            with open(os.path.join(pth,'{}.csv'.format(token)), 'w', newline='') as f:
+                writerc = csv.writer(f)
+                if withaggtrades == False:
+                    dic = ['date', 'open', 'high','low', 'close','volume','symbol','QAV','numberoftrades','takerbuyBAV','takerbuyQAV','market_cap', 'total_volume','price_from_coingecko', 'factor']
+                    writerc.writerow(dic)
+                gct = 0
+                it = 0
+                prevdate = 0
+                testvar = len(klines)//60
+                testvar2 = (len(klines)+ 60)//60
+                if type == '1min':
+                    if len(d1) > (len(klines)+ 60)//60:
+                        cnct = len(d1) - ((len(klines)//60) + 1)
+                        mnx = 0
+                    elif len(d1) == (len(klines) + 60)//60:
+                        cnct = 0 
+                        mnx = 0
+                    
+                    elif len(d1) == len(klines)//60:
+                        cnct = 0 
+                        mnx = 0
+                    else:
+                        cnct = 0
+                        mnx = len(klines)//60 - len(d1) 
+                if type == 'day':
+                    if len(d1) > (len(klines) + 1):
+                        cnct = len(d1) - ((len(klines)) + 1)
+                        mnx = 0
+                    elif len(d1) == (len(klines) + 1):
+                        cnct = 0 
+                        mnx = 0
+                    
+                    elif len(d1) == len(klines):
+                        cnct = 0 
+                        mnx = 1
+                    else:
+                        cnct = 0
+                        mnx = (len(klines) + 1) - len(d1) 
+                templist = []
+                for a in klines: 
+                    if withaggtrades == True:
+                        olis = []
+                        if type == 'day':
+                            agg_trades = client.aggregate_trade_iter(symbol=token, start_str=a[0], endingm=a[6], day=True)
+                        if type == '1min':
+                            agg_trades = client.aggregate_trade_iter(symbol=token, start_str=a[0], endingm=a[6], day=False)
+                        dic = ['date', 'open', 'high','low', 'close','volume','symbol','QAV','numberoftrades','takerbuyBAV','takerbuyQAV','market_cap', 'total_volume','price_from_coingecko', 'factor']
+                        
+                        for enumr, qqq in enumerate(range(1000)):
+                            dic.append('p_{}'.format(enumr))
+                            dic.append('q_{}'.format(enumr))
+                            dic.append('m_{}'.format(enumr))
+                            dic.append('M_{}'.format(enumr))
+                        writerc.writerow(dic)
+                        for bb in agg_trades:
+                            for bbb in bb:
+                                if bbb == []:
+                                    continue
+                                for bbbbb in bbb:
+                                    olis.append(bbbbb)
+                        fnlis = sorted(olis, key = lambda i: i['q'], reverse=True)[0:1000]
+                    if type == 'day':
+                        if datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%d") != prevdate:
+                            cnct = cnct + 1
+                            prevdate = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%d")
+                            if mnx != None:
+                                if mnx != 0:
+                                    mnx = mnx -1
+                                    cnct = cnct -1
+                        dd = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%Y-%m-%d")
+                        b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],d1[cnct],d2[cnct],d3[cnct],1]
+                        if withaggtrades == True:
+                            for enumr, qqq in enumerate(range(1000)):
+                                try:
+                                    b.append(fnlis[enumr]['p'])
+                                    b.append(fnlis[enumr]['q'])
+                                    b.append(fnlis[enumr]['m'])
+                                    b.append(fnlis[enumr]['M'])
+                                except:
+                                    b.append(0)
+                                    b.append(0)
+                                    b.append(0)
+                                    b.append(0)
+                        templist.append(b)
+                        gct = gct + 1
+                        if gct == 10000:
+                            it = it + 1
+                            gct= 0
+                            print('{} Iteration #: '.format(token), it)
+                            writerc.writerows(templist)
+                            templist = []
+                        writerc.writerows(templist)
+                        templist = []
+                    if type == '1min':
+                        dte = int(datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%H"))
+                        if  dte != prevdate:
+                            cnct = cnct + 1
+                            prevdate = int(datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%H"))
+                            if mnx != None:
+                                if mnx != 0:
+                                    mnx = mnx -1
+                                    cnct = cnct -1
+                        dd = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%Y-%m-%d %H:%M:%S")
+                        b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],d1[cnct],d2[cnct],d3[cnct],1]
+                        if withaggtrades == True:
+                            for enumr, qqq in enumerate(range(1000)):
+                                try:
+                                    b.append(fnlis[enumr]['p'])
+                                    b.append(fnlis[enumr]['q'])
+                                    b.append(fnlis[enumr]['m'])
+                                    b.append(fnlis[enumr]['M'])
+                                except:
+                                    b.append(0)
+                                    b.append(0)
+                                    b.append(0)
+                                    b.append(0)
+                    
+                    
+                        templist.append(b)
+                        gct = gct + 1
+                        if gct == 10000:
+                            it = it + 1
+                            gct= 0
+                            print('{} Iteration #: '.format(token), it)
+                            writerc.writerows(templist)
+                            templist = []
+                        writerc.writerows(templist)
+                        templist = []
+        else:
+            with open(os.path.join(pth,'{}.csv'.format(token)), 'w', newline='') as f:
+                writerc = csv.writer(f)
+                dic = ['date', 'open', 'high','low', 'close','volume','symbol','QAV','numberoftrades','takerbuyBAV','takerbuyQAV','factor']
+                writerc.writerow(dic)
+                gct = 0
+                it = 0
+                prevdate = 0
+                for a in klines:
+                    if datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%d") != prevdate:
+                        cnct = cnct + 1
+                        prevdate = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%d")
+                    
+                    if type == 'day':
+                        dd = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%Y-%m-%d")
+                    if type == '1min':
+                        dd = datetime.fromtimestamp(a[0]/1000.0,tz=pytz.utc).strftime("%Y-%m-%d %H:%M:%S")
+                    b = [dd, a[1], a[2], a[3], a[4], a[5],token,a[7],a[8],a[9],a[10],1]
+                    writerc.writerow(b)
+                    gct = gct + 1
+                    if gct == 1000:
+                        it = it + 1
+                        gct= 0
+                        print('{} Iteration #: '.format(token), it)
+                writerc.writerows(templist)
+           
+            
 
 
 def takehalf(v, list):
@@ -176,33 +360,40 @@ def takehalf(v, list):
         second_half = list[middle_index:]
         return second_half
 
-def startdownload_day(ver):
-    
-    dirs = os.path.dirname(os.path.realpath(__file__))
-    pth = os.path.join(dirs, 'data-download/day-unprocessed')
-    exchg = get_exchange_info()
-    currlist = getstate(checkprog(pth),exchg)
+def startdownload_day(withcgdata=True, withaggtrades=False, aggtradelimit=0, run=True, ver=None):
+    if run == True:
+        dirs = os.path.dirname(os.path.realpath(__file__))
+        if withcgdata == True:
+            pth = os.path.join(dirs, 'data-download/day-unprocessed')
+        else:
+            pth = os.path.join(dirs, 'data-download/day-processed')
+        exchg = get_exchange_info()
+        currlist = getstate(checkprog(pth),exchg)
+        if not ver == None:
+            currlist = takehalf(ver, currlist)
 
-    currlist = takehalf(ver, currlist)
+        start = "21 Dec, 2010"
+        end = "5 Jan, 2022"
+        interval = Client.KLINE_INTERVAL_1DAY
+        download(start,end,interval,currlist,pth,'day',withaggtrades=False,aggtradelimit=0)
 
-    start = "21 Dec, 2010"
-    end = "21 Dec, 2021"
-    interval = Client.KLINE_INTERVAL_1DAY
-    download(start,end,interval,currlist,pth,'day')
+def startdownload_1min(withcgdata=True, withaggtrades=False, aggtradelimit=0, run=False, ver=None):
+    if run == True:
+        dirs = os.path.dirname(os.path.realpath(__file__))
+        if withcgdata == True:
+            pth = os.path.join(dirs, 'data-download/1min-unprocessed')
+        else:
+            pth = os.path.join(dirs, 'data-download/1min-processed')
+        exchg = get_exchange_info()
+        currlist = getstate(checkprog(pth),exchg)
 
-def startdownload_1min(ver):
-    
-    dirs = os.path.dirname(os.path.realpath(__file__))
-    pth = os.path.join(dirs, 'data-download/1min-unprocessed')
-    exchg = get_exchange_info()
-    currlist = getstate(checkprog(pth),exchg)
+        if not ver == None:
+            currlist = takehalf(ver, currlist)
 
-    currlist = takehalf(ver, currlist)
-
-    start = "21 Dec, 2010 0:00"
-    end = "21 Dec, 2021 0:00"
-    interval = Client.KLINE_INTERVAL_1MINUTE
-    download(start,end,interval,currlist,pth,'1min')
+        start = "21 Dec, 2010 0:00"
+        end = "5 Jan, 2022 0:00"
+        interval = Client.KLINE_INTERVAL_1MINUTE
+        download(start,end,interval,currlist,pth,'1min',withaggtrades=withaggtrades,aggtradelimit=aggtradelimit)
 
 
     
@@ -233,9 +424,7 @@ def interval_to_milliseconds(interval):
     return ms
 
 # requires dateparser package
-import dateparser
-import pytz
-from datetime import datetime
+
 
 
 
